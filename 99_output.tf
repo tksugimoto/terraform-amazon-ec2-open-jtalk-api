c8380e254c3a41503ddb550812930b34b2548ea1
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-12-19 00:03:32 +0900

    [Update] Output: Open JTalk Web API の URL(サンプル)を出力

--- 99_output.tf
+++ 99_output.tf
@@ -5,3 +5,7 @@ output "Server IP" {
 output "Server SSH Port" {
   value = "${var.ssh_port}"
 }
+
+output "Open JTalk Web API URL (Sample)" {
+  value = "http://${aws_instance.main.public_ip}:${var.http_port}/voice?text=こんにちは"
+}
